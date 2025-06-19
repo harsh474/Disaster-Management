57fa09dd4324d9bfbbc1063d5d346c3aea0ac633
Harsh Rajput <harshrajput1101@gmail.com>
2025-06-19 20:01:08 +0530

    create post request

--- src/controllers/disasters.js
+++ src/controllers/disasters.js
@@ -1,13 +1,28 @@
 import supabase from "../config/db.js";
 
 
-export const create_disater = (req, res) => {
-  // TODO: Implement create disaster 
-  supabase.from('disaters').insert({ 
-      title:req.body.title , 
-      
-  })
-  res.status(501).send('Not implemented');
+export const create_disater = async (req, res) => {
+  // TODO: Implement create disaster  
+    const { title, description, location_name, lat, lng, tags, owner_id } = req.body ;  
+   
+    let  audit_trail = [{
+        action: 'create',
+        user_id: owner_id,
+        timestamp: new Date().toISOString()
+      }]
+    const location = `POINT(${lat} ${lng})` ; 
+    try {  
+    let {data,error }=  await supabase.from('disaters').insert(
+    {
+      title, description, location_name,location , tags, owner_id,audit_trail
+    }) 
+    if(data) res.status(200).send(data)
+    else res.status(400).send(error)
+    } catch (error) {
+       res.status(400).send(error);
+    }
+  
+  
 };
 
 export const get_disaters = (req, res) => {
